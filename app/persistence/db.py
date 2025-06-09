9b305d4de7b1d22c87e532daf48d21bb1ce64ad3
technicolo <franfrare19@gmail.com>
2025-06-09 19:07:54 -0300

    optional features 6/26  26.Use a database migration tool (e.g Alembic)

--- app/persistence/db.py
+++ app/persistence/db.py
@@ -1,7 +1,8 @@
 from sqlmodel import SQLModel, create_engine, Session
 
 DATABASE_URL = "sqlite:///./ecommerce.db"
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine("sqlite:///ecommerce.db", echo=True)
+
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
